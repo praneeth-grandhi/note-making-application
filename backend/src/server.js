7e972b7bf4409958e886cdb6232775c78a6bd1fa
Praneeth Grandhi <praneeth.gsk@gmail.com>
2025-10-23 19:37:24 +0530

    Made deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,6 +27,7 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get(/.*/, (req, res) => {
